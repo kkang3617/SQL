d49a9af2562b88b544d91907ccdbc6f21db1046e
kkang3617 <rkdgus3617@naver.com>
2023-04-19 18:12:14 +0900

    DAY43 04.19.

--- 10_insert_update_delete.sql
+++ 10_insert_update_delete.sql
@@ -47,3 +47,55 @@ CTAS를 사용하면 제약 조건은 NOT NULL 말고는 복사되지 않는다.
 DB에 저장되는 것을 방지하는 목적으로 사용한다.
 */
 
+
+-- UPDATE를 진행할 때는 누구를 수정할 지 잘 지목해야 한다.
+-- 그렇지 않으면 수정 대상이 테이블 전체로 지목된다.
+UPDATE emps SET salary = 30000; --WHERE 조건 지정해주지 않으면 모든값이 
+                                --30000으로 변경된다.
+SELECT * FROM emps;
+ROLLBACK;
+
+UPDATE emps SET salary = 30000
+WHERE employee_id = 100;
+
+UPDATE emps SET salary = salary + salary * 0.1
+WHERE employee_id = 100;
+
+
+UPDATE emps SET 
+phone_number = '010.1234.1234' , manager_id = 102
+WHERE employee_id = 100;
+
+
+-- UPDATE (서브쿼리)
+UPDATE emps
+    SET (job_id, salary, manager_id) =
+    (SELECT job_id, salary, manager_id  --사번이 100번인 사람 값들 조회해서
+    FROM emps                           
+    WHERE employee_id = 100
+    )
+WHERE employee_id = 101; -- 사번이 101번인 사람에게 적용
+
+
+-- DELETE
+DELETE FROM emps   -- DELETE * FROM ~~~ 으로 쓰는 실수를 자주함!! * 쓰지말것!
+WHERE employee_id = 103;
+
+SELECT * FROM emps
+WHERE employee_id = 103; -- 삭제해서 조회결과가 없음.
+
+
+-- 사본테이블 생성
+CREATE TABLE depts AS (SELECT * FROM departments);
+SELECT * FROM depts;
+
+
+-- DELETE (서브쿼리)
+DELETE FROM emps
+WHERE department_id = (SELECT department_id FROM depts
+                        WHERE department_name = 'IT');
+
+
+
+
+
